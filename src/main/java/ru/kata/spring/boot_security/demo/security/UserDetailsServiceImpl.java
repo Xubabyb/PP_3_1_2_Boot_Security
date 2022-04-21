d0c52554aab9bac31cf2e4b448a15d862d659def
DmitriyShidlouskij <xubabyb@gmail.com>
2022-04-21 22:29:54 +0300

    add UserDetailsServiceImpl

---
+++ src/main/java/ru/kata/spring/boot_security/demo/security/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package ru.kata.spring.boot_security.demo.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.repository.UserRepository;
+
+@Service("userDetailsServiceImpl")
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("user not exist"));
+        return user.fromUser();
+    }
+}
